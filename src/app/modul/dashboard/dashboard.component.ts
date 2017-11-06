54c65f2e4591e844446365bde137db6cfe6b1dc1
romli <roemly@gmail.com>
2017-11-06 09:24:38 +0700

    paksa login

--- src/app/modul/dashboard/dashboard.component.ts
+++ src/app/modul/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Post} from '../../class/Post';
 import {PostService} from '../../service/post.service';
+import {Router} from '@angular/router';
+import {LoginService} from '../../service/login.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +11,10 @@ import {PostService} from '../../service/post.service';
 })
 
 export class DashboardComponent {
-  constructor(private posts: PostService){ }
+  constructor(private posts: PostService, private router: Router, private login: LoginService){
+    if (!login.isLogin()) {
+      this.router.navigate(['login']);
+    }
+   }
 }
 
