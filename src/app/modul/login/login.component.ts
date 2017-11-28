0da5facda222fa7eb0e5e6113adf3149dfcc2877
romli <roemly@gmail.com>
2017-11-28 15:11:49 +0700

    perbaiki login

--- src/app/modul/login/login.component.ts
+++ src/app/modul/login/login.component.ts
@@ -11,19 +11,23 @@ import {LoginService} from '../../service/login.service';
 export class LoginComponent {
   email= '';
   pass= '';
-  message = 'test';
+  message = '';
   constructor(
       public router: Router,
       public users: LoginService
   ){}
 
   onClick (){
-        console.log();
+        
       this.users.attempt(this.email, this.pass).then(t => {
+        //console.log(t._body);
+
         if(t._body === "[]"){
             this.message = 'username atau password salah!';
         }else {
-            this.router.navigate(['dashboard']);
+          console.log('berhasil');
+          localStorage.user = JSON.stringify(t._body);
+          this.router.navigate(['dashboard']);
         }
       });
   }
